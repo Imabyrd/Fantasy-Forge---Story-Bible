d20dffdbb3615bb22a2b1717daa2b2d9fbe8e095
ImaByrd <emma.a.brannen@gmail.com>
2024-05-06 04:41:38 -0400

    Initial Commit

---
+++ Fantasy Forge - Story Scribe/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Fantasy Forge - Story Scribe
+//
+//  Created by Emma Brannen on 5/6/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
